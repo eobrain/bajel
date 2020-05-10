1632d18a23f48f5f577e6df7b0b5ba472f6a7954
Eamonn O'Brien-Strain <eobrain@gmail.com>
2020-05-10 16:27:52 -0700

    Refactor recurse into tasks

--- src/index.js
+++ src/index.js
@@ -56,76 +56,8 @@ module.exports = async (bajelfile) => {
     ? options._[0]
     : tasks.explicitTargets()[0]
 
-  /**
- * @param {string} target being built
- * @returns {[errorCode, number, recipeHappened]} whether succeeded and timestamp in ms of latest file change
- * */
-  const recurse = async (prevTargets, target) => {
-    const debugOut = f => {
-      if (options.debug) {
-        tConsole.log(`target "${target}" ${f()}`)
-      }
-    }
-
-    let recipeHappened = false
-    const targetTime = await timestamp(target)
-    if (!tasks.has(target) && targetTime === 0) {
-      tConsole.warn(target,
-        'is not a file and is not one of the build targets:',
-        tasks.targets().sort())
-      return [1]
-    }
-    const task = tasks.get(target)
-
-    // Check for recursion
-    if (prevTargets.includes(target)) {
-      throw new Error(`infinite loop ${prevTargets.join(' → ')} → ${target}`)
-    }
-    prevTargets = [...prevTargets, target]
-
-    let lastDepsTime = 0
-    for (const dep of task.theDeps()) {
-      const [depCode, depTime, depRecipeHappened] = await recurse(prevTargets, dep)
-      recipeHappened = recipeHappened || depRecipeHappened
-      if (depCode !== 0) {
-        debugOut(() => `-- execution of dep target "${dep}" failed. Stopping.`)
-        return [depCode]
-      }
-      if (depTime > lastDepsTime) {
-        lastDepsTime = depTime
-      }
-    }
-
-    debugOut(() => `${ago(targetTime)} and its most recent deps ${ago(lastDepsTime)}`)
-    if (task.hasRecipe() && (targetTime === 0 || targetTime < lastDepsTime)) {
-      debugOut(() => targetTime === 0
-        ? 'does not exist and has a recipe'
-        : 'is older than the most recent dep and has a recipe'
-      )
-      const callHappened = task.doCall(dryRun, tConsole)
-      recipeHappened = recipeHappened || callHappened
-      if (!callHappened) {
-        const code = await task.doExec(variables, dryRun, tConsole)
-        recipeHappened = true
-        if (code !== 0) {
-          tConsole.error('FAILED call', task.toString())
-          return [code]
-        }
-      }
-    } else {
-      debugOut(() => !task.hasRecipe()
-        ? 'has no recipe'
-        : (lastDepsTime === 0
-          ? 'exists and there are no deps so ignoring recipe'
-          : 'is more recent than the most recent dep so ignoring recipe'
-        )
-      )
-    }
-    const updatedTime = Math.max(lastDepsTime, await timestamp(target))
-    return [0, updatedTime, recipeHappened]
-  }
-
   try {
+    // Phase 1: Expansion
     while (tasks.expandDeps(tConsole)) { }
   } catch (e) {
     tConsole.error('Problem expanding percents: ' + e)
@@ -144,7 +76,8 @@ module.exports = async (bajelfile) => {
   }
 
   try {
-    const [code, ts, recipeHappened] = await recurse([], start)
+    // Phase 2: Executions
+    const [code, ts, recipeHappened] = await tasks.recurse([], start, variables, dryRun, options.debug)
 
     if (code !== 0) {
       tConsole.error(`bajel: recipe for target '${start}' failed\nbajel: *** [error] Error ${code}`)
--- src/tasks.js
+++ src/tasks.js
@@ -1,17 +1,18 @@
 const Task = require('./task.js')
-const { walkDir } = require('./fs_util.js')
+const { timestamp, walkDir } = require('./fs_util.js')
+const ago = require('./ago.js')
 
 module.exports = class {
   constructor (bajelfile, tConsole) {
     this._tConsole = tConsole
-    this._dict = {}
+    this._tasks = {}
     for (const key in bajelfile) {
       const value = bajelfile[key]
       if (typeof value === 'object' && !Array.isArray(value)) {
-        this._dict[key] = new Task(key, value)
+        this._tasks[key] = new Task(key, value)
       }
     }
-    this._explicitTargets = Object.keys(this._dict).filter(k => !k.includes('%'))
+    this._explicitTargets = Object.keys(this._tasks).filter(k => !k.includes('%'))
   }
 
   explicitTargets () {
@@ -19,35 +20,35 @@ module.exports = class {
   }
 
   has (target) {
-    return !!this._dict[target]
+    return !!this._tasks[target]
   }
 
   get (target) {
-    return this._dict[target] || new Task({}, this._tConsole)
+    return this._tasks[target] || new Task({}, this._tConsole)
   }
 
   targets () {
-    return Object.keys(this._dict)
+    return Object.keys(this._tasks)
   }
 
   _forTask (callback) {
-    for (const target in this._dict) {
-      callback(this._dict[target])
+    for (const target in this._tasks) {
+      callback(this._tasks[target])
     }
   }
 
   _removeAll (toRemove) {
-    toRemove.forEach(target => { delete this._dict[target] })
+    toRemove.forEach(target => { delete this._tasks[target] })
   }
 
   _addAll (toAdd) {
     for (const target in toAdd) {
-      if (this._dict[target]) {
+      if (this._tasks[target]) {
         this._tConsole.warn('Duplicate targets')
-        this._tConsole.warn(this._dict[target].toString())
+        this._tConsole.warn(this._tasks[target].toString())
         this._tConsole.warn(toAdd[target].toString())
       }
-      this._dict[target] = toAdd[target]
+      this._tasks[target] = toAdd[target]
     }
   }
 
@@ -85,5 +86,74 @@ module.exports = class {
     this._removeAll(toRemove)
     this._addAll(toAdd)
     return expansionHappened
+  }
+
+  /**
+ * @param {string} target being built
+ * @returns {[errorCode, number, recipeHappened]} whether succeeded and timestamp in ms of latest file change
+ * */
+  async recurse (prevTargets, target, variables, dryRun, debug) {
+    const debugOut = f => {
+      if (debug) {
+        this._tConsole.log(`target "${target}" ${f()}`)
+      }
+    }
+
+    let recipeHappened = false
+    const targetTime = await timestamp(target)
+    if (!this.has(target) && targetTime === 0) {
+      this._tConsole.warn(target,
+        'is not a file and is not one of the build targets:',
+        this.targets().sort())
+      return [1]
+    }
+    const task = this.get(target)
+
+    // Check for recursion
+    if (prevTargets.includes(target)) {
+      throw new Error(`infinite loop ${prevTargets.join(' → ')} → ${target}`)
+    }
+    prevTargets = [...prevTargets, target]
+
+    let lastDepsTime = 0
+    for (const dep of task.theDeps()) {
+      const [depCode, depTime, depRecipeHappened] = await this.recurse(prevTargets, dep, variables, dryRun, debug)
+      recipeHappened = recipeHappened || depRecipeHappened
+      if (depCode !== 0) {
+        debugOut(() => `-- execution of dep target "${dep}" failed. Stopping.`)
+        return [depCode]
+      }
+      if (depTime > lastDepsTime) {
+        lastDepsTime = depTime
+      }
+    }
+
+    debugOut(() => `${ago(targetTime)} and its most recent deps ${ago(lastDepsTime)}`)
+    if (task.hasRecipe() && (targetTime === 0 || targetTime < lastDepsTime)) {
+      debugOut(() => targetTime === 0
+        ? 'does not exist and has a recipe'
+        : 'is older than the most recent dep and has a recipe'
+      )
+      const callHappened = task.doCall(dryRun, this._tConsole)
+      recipeHappened = recipeHappened || callHappened
+      if (!callHappened) {
+        const code = await task.doExec(variables, dryRun, this._tConsole)
+        recipeHappened = true
+        if (code !== 0) {
+          this._tConsole.error('FAILED call', task.toString())
+          return [code]
+        }
+      }
+    } else {
+      debugOut(() => !task.hasRecipe()
+        ? 'has no recipe'
+        : (lastDepsTime === 0
+          ? 'exists and there are no deps so ignoring recipe'
+          : 'is more recent than the most recent dep so ignoring recipe'
+        )
+      )
+    }
+    const updatedTime = Math.max(lastDepsTime, await timestamp(target))
+    return [0, updatedTime, recipeHappened]
   }
 }
